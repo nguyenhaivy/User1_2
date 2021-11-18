a7bc7931fb271a042d170360594fc30372f4d2b7
nguyenhaivy <93856700+nguyenhaivy@users.noreply.github.com>
2021-11-18 11:51:02 +0700

    kiemtrachanle

---
+++ sourchCL.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+bool kiemTraCL(int a)
+{
+	if (a %2 == 0)
+{
+	return true;
+}
+return false;
+}
+int main()
+{
+	int a;
+	if(kiemTraCL == "true")
+	cout<<"so"<<a <<"la so chan";
+	else if(kiemTraCL == "")alse
+	cout<<"so"<<a <<"la so le";
+
+	system("pause");
+	return 0;
+}
+
