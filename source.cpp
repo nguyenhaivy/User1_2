10cb08fcce47a00e4e5e51000c14370207c63d77
nguyenhaivy <93856700+nguyenhaivy@users.noreply.github.com>
2021-11-18 11:00:50 +0700

    Them tong tich hieu

---
+++ source.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int tong(int a, int b)
+{
+	return a+b;
+}
+int hieu(int a, int b)
+{
+	return a-b;
+}
+int tich (int a , int b)
+{	
+	return a*b;
+}
+
+int main()
+{
+	int a, b;
+	char phepToan;
+	cout << "Nhap so nguyen 1: ";
+	cin >> a;
+	cout << "Nhap so nguyen 2: ";
+	cin >> b;
+	cout << "Chon phep toan (+,-) ";
+	cin >> phepToan;
+if(phepToan = '_')
+	cout<<"Hieu = " << hieu(a,b)<<endl;
+if(phepToan = '+')
+	cout<<"Tong = " << Tong(a,b)<<endl;
+if(phepToan = '*')
+	cout<<"Tich = " << Tich(a,b)<<endl;
+
+	system("pause");
+	return 0;
+}
